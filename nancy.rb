cc30bc7a3447509b9b7dd7cfbc1ebd903243c276
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-10-10 11:35:20 -0400

    Add a minimal call method to run the app

--- nancy.rb
+++ nancy.rb
@@ -8,6 +8,8 @@ require "awesome_print"
 # For each request, it will match the requested route to the stored routes, and
 # take action if there’s a match, or return a 404 if nothing matches.
 
+# Block handlers must return something that Rack can understand.
+
 module Nancy
   class Base
 
@@ -21,16 +23,34 @@ module Nancy
       route("GET", path, &handler)
     end
 
+    # To make Nancy::Base a Rack app
+    def call(env)
+      # Grab the verb and requested path from the env parameter
+      @request = Rack::Request.new(env)
+      verb           = @request.request_method
+      requested_path = @request.path_info
+
+      # Grab the handler block from @routes
+      handler = @routes[verb][requested_path]
+
+      handler.call
+    end
+
     private
 
+      # Store routes as handler blocks
       def route(verb, path, &handler)
+        # Create a hash for the verb if it is the first time
         @routes[verb] ||= {}
         @routes[verb][path] = handler
       end
   end
 end
 
-#==> TEST - Run ruby nancy.rb
+#==> TEST
+# 1. Run `ruby nancy.rb`
+# 2. Visit http://localhost:9292/hello
+# 3. Hit Ctrl-c to quit
 
 # Instantiate the Nancy::Base class
 nancy = Nancy::Base.new
@@ -40,5 +60,8 @@ nancy.get "/hello" do
   [200, {}, ["Nancy says hello"]]
 end
 
-# Print the routes
+# Print all the routes
 ap nancy.routes
+
+# Run the app using the server WEBrick that is built in to Ruby.
+Rack::Handler::WEBrick.run nancy, Port: 9292
