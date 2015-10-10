46499430ff3de2ee68c7f740f5737fcc031df759
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-10-10 11:07:54 -0400

    Implement basic structure

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # simple_rack_app
+
+- For me to understand how Rack applications work by building a simple Sinatra-like Rack app.
+- All is based on the article ["Let's Build a Sinatra"](https://robots.thoughtbot.com/lets-build-a-sinatra) by Gabe Berke-Williams. Thank you so much.
+
+## Rack
+- specifies a standard interface for Ruby webservers
+- [Advanced Rack](http://gabebw.com/blog/2015/08/10/advanced-rack) by Gabe Berke-Williams
+
+## Sinatra
+- a layer on top of Rack
+- DSL for specifying what a Rack app responds to, and what it sends back
+- Enable us to quickly create web applications in Ruby 
+- [source](https://github.com/sinatra/sinatra/blob/master/lib/sinatra/base.rb)
+
+
+
+
--- nancy.rb
+++ nancy.rb
@@ -1,7 +1,16 @@
 require "rack"
+require "awesome_print"
+
+# A base Sinatra-like class that other classes can inherit from.
+# https://robots.thoughtbot.com/lets-build-a-sinatra
+
+# Store routes (like GET /hello) and actions to take when hitting those routes.
+# For each request, it will match the requested route to the stored routes, and
+# take action if there’s a match, or return a 404 if nothing matches.
 
 module Nancy
   class Base
+
     def initialize
       @routes = {}
     end
@@ -21,12 +30,15 @@ module Nancy
   end
 end
 
+#==> TEST - Run ruby nancy.rb
+
+# Instantiate the Nancy::Base class
 nancy = Nancy::Base.new
 
+# Add a GET request route
 nancy.get "/hello" do
   [200, {}, ["Nancy says hello"]]
 end
 
-puts nancy.routes
-
-# run ruby nancy.rb
+# Print the routes
+ap nancy.routes
